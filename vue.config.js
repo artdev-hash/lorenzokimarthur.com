81c25c17f74799d53998e942bf556d7cd6b9cbd0
artdev-hash <kim.dev.pr@gmail.com>
2022-09-09 22:41:54 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/lorenzokimarthur.com/',
+  publicPath: '/lorenzokimarthur.com/',
   transpileDependencies: true
 })
