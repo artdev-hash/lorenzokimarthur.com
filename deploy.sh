679681ed47a037960b15f895deccdedb4d651a3c
artdev-hash <kim.dev.pr@gmail.com>
2022-09-09 18:57:07 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,13 @@ npm run build
 
 cd dist
 
+# if you are deploying to a custom domain
+echo 'www.lorenzokimarthur.com' > CNAME
+
 git init
 git add -A 
 git commit -m 'New Deployment'
+
 git push -f git@github.com:artdev-hash/lorenzokimarthur.com.git master:gh-pages
 
 cd -
