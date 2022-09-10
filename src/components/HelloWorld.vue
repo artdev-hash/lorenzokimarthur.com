03a1b40ea6db443c7fec2a36c22f81281179ce52
artdev-hash <kim.dev.pr@gmail.com>
2022-09-10 22:00:38 +0800

    working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>arthurdev</p>
+    <p>twitter: @devkimlorenzo</p>
+    <p>github/artdev-hash</p>
       <!--<a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
