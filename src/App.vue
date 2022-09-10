4c91d764783edf7eeb754ee62bf74f91450d7152
artdev-hash <kim.dev.pr@gmail.com>
2022-09-11 01:42:38 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Direct Message for IT Support Services"/>
+  <img alt="vue" src="./assets/vuejs-icon.svg" width="40" height="40">
+  <img alt="gitscm" src="./assets/git-scm-icon.svg" width="40" height="40">
+  <img alt="githhub" src="./assets/github-icon.svg" width="40" height="40">
+  <img alt="kraken" src="./assets/gitkraken-icon.svg" width="40" height="40">
+
+
+  <HelloWorld msg="Thank you for visiting"/>
 </template>
 
 <script>
