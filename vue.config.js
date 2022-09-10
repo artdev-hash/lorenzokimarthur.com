f074939e1b4c7dff6a6b18339205245478557598
artdev-hash <kim.dev.pr@gmail.com>
2022-09-10 21:41:12 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'development'
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/lorenzokimarthur.com/'
     : '/',
   outputDir: 'dist',
